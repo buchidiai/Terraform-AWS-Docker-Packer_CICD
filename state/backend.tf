50ef5407c28ef92fbc4a6704111e3a6b4a0908ba
buchidiai <31743169+buchidiai@users.noreply.github.com>
2023-03-08 18:31:17 -0600

    added config for aws bucket w/ versioning,encryption & dynodb table

--- state/backend.tf
+++ state/backend.tf
@@ -13,7 +13,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "terraform_state" {
+resource "aws_s3_bucket_versioning" "terraform_state" {
 
   bucket = "terraform-code-bucket"
 
@@ -23,16 +23,38 @@ resource "aws_s3_bucket" "terraform_state" {
 
   # Enable versioning so we can see the full revision history of our
   # state files
-  versioning {
-    enabled = true
+  versioning_configuration {
+    status = "Enabled"
   }
 
-  # Enable server-side encryption by default
-  server_side_encryption_configuration {
-    rule {
+
+}
+
+resource "aws_kms_key" "js_sdsdnk_bjsdu829-" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+}
+
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = aws_s3_bucket_versioning.terraform_state.id
+
+   # Enable server-side encryption by default
+   rule {
       apply_server_side_encryption_by_default {
         sse_algorithm = "AES256"
       }
     }
+}
+
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-locks-table"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
   }
 }
