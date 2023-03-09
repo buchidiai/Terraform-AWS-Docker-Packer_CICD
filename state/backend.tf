7f7657b7d00679979e350c0f4417bed5e43d62f1
buchidiai <31743169+buchidiai@users.noreply.github.com>
2023-03-08 18:44:47 -0600

    added backend

--- state/backend.tf
+++ state/backend.tf
@@ -9,14 +9,53 @@ terraform {
   required_version = "1.3.9" # or latest Terraform version
 }
 
-provider "aws" {
-  region = "us-east-1"
+variable "bucket_name" {
+  description = "bucket name"
+  default = "terra-bucket-01"
+  type = string
 }
 
-resource "aws_s3_bucket_versioning" "terraform_state" {
+variable "dyno_table_name" {
+  description = "terraform state table name"
+  default = "terraform-locks-table"
+  type = string
+}
 
-  bucket = "terraform-code-bucket"
+variable "region_name" {
+  description = "region name"
+  default = "us-east-1"
+  type = string
+}
 
+provider "aws" {
+  region = var.region_name
+}
+
+
+
+
+terraform {
+	backend "s3" {
+		bucket = "terra-bucket-01"
+		key = "global/s3/terraform.tfstate"
+		region = "us-east-1"
+
+		dynamodb_table = "terraform-locks-table"
+		encrypt = true
+	}
+}
+
+resource "aws_s3_bucket" "state_backend" {
+  bucket = var.bucket_name
+}
+
+resource "aws_s3_bucket_acl" "state_backend" {
+  bucket = aws_s3_bucket.state_backend.id
+  acl    = "private"
+}
+resource "aws_s3_bucket_versioning" "versioning_example" {
+
+  bucket = aws_s3_bucket.state_backend.id
   lifecycle {
   	prevent_destroy = true
   }
@@ -37,7 +76,7 @@ resource "aws_kms_key" "js_sdsdnk_bjsdu829-" {
 
 
 resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
-  bucket = aws_s3_bucket_versioning.terraform_state.id
+  bucket = aws_s3_bucket_versioning.versioning_example.id
 
    # Enable server-side encryption by default
    rule {
@@ -49,7 +88,7 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
 
 
 resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform-locks-table"
+  name         = var.dyno_table_name
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
