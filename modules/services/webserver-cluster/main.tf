af11cbabd01dd755172ddde62e50757999d1e440
buchidiai <31743169+buchidiai@users.noreply.github.com>
2023-03-11 17:48:35 -0600

    add local variable

--- modules/services/webserver-cluster/main.tf
+++ modules/services/webserver-cluster/main.tf
@@ -1,20 +1,22 @@
-#variable for port
-variable "server_port" {
-  description = "The port the server will be listening on"
-  default = 8080
-  type = number
+locals {
+  http_port    = 80
+  any_port     = 0
+  any_protocol = "-1"
+  tcp_protocol = "tcp"
+  all_ips      = ["0.0.0.0/0"]
+  server_port = 8080
 }
 
 #security group with ingress & egress ports setting
 resource "aws_security_group" "instance" {
-  name = "${var.cluster_name}-instance"
+  name        = "${var.cluster_name}-instance"
   description = "default traffic"
 
   ingress {
-    from_port   = var.server_port
-    to_port     = var.server_port
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = local.server_port
+    to_port     = local.server_port
+    protocol    = local.tcp_protocol
+    cidr_blocks = local.all_ips
   }
 }
 
@@ -24,18 +26,18 @@ resource "aws_security_group" "alb" {
 
   # Allow inbound HTTP requests
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = local.http_port
+    to_port     = local.http_port
+    protocol    = local.tcp_protocol
+    cidr_blocks = local.all_ips
   }
 
   # Allow all outbound requests
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = local.any_port
+    to_port     = local.any_port
+    protocol    = local.any_protocol
+    cidr_blocks = local.all_ips
   }
 }
 
@@ -57,17 +59,17 @@ data "aws_ami" "ubuntu" {
 
 # spin up ec2 instances w/ security group in auto scaling group
 resource "aws_launch_configuration" "ec2-grp" {
-  image_id = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
+  image_id        = data.aws_ami.ubuntu.id
+  instance_type   = var.instance_type
   security_groups = [aws_security_group.instance.id]
 
   user_data = <<-EOF
     #!/bin/bash
     echo "Hello, World" > index.html
-    nohup busybox httpd -f -p ${var.server_port} &
+    nohup busybox httpd -f -p ${local.server_port} &
     EOF
-#when Terraform must change a resource,
-#Terraform will instead destroy the existing object and then create a new replacement object with the new configured arguments
+  #when Terraform must change a resource,
+  #Terraform will instead destroy the existing object and then create a new replacement object with the new configured arguments
   lifecycle {
     create_before_destroy = true
   }
@@ -83,14 +85,14 @@ data "aws_vpc" "default" {
 #get subnet ids by using the default vpc
 data "aws_subnets" "subnets" {
   filter {
-     name   = "vpc-id"
-     values = [data.aws_vpc.default.id]
-   }
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
 }
 # configuration for the auto scaling group
 resource "aws_autoscaling_group" "ec2-grp" {
   launch_configuration = aws_launch_configuration.ec2-grp.name
-  vpc_zone_identifier = data.aws_subnets.subnets.ids
+  vpc_zone_identifier  = data.aws_subnets.subnets.ids
 
   target_group_arns = [aws_lb_target_group.asg.arn]
   health_check_type = "ELB"
@@ -99,23 +101,23 @@ resource "aws_autoscaling_group" "ec2-grp" {
   max_size = var.max_size
 
   tag {
-    key = "Name"
-    value = "${var.cluster_name}-asg"
+    key                 = "Name"
+    value               = "${var.cluster_name}-asg"
     propagate_at_launch = true
   }
 }
 # configuration for the load balancer
 resource "aws_lb" "ec2-grp" {
-  name = "${var.cluster_name}-alb"
+  name               = "${var.cluster_name}-alb"
   load_balancer_type = "application"
-  subnets = data.aws_subnets.subnets.ids
-  security_groups = [aws_security_group.alb.id]
+  subnets            = data.aws_subnets.subnets.ids
+  security_groups    = [aws_security_group.alb.id]
 }
 #config for listener rule for appliaction load balancer
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.ec2-grp.arn
-  port = 80
-  protocol = "HTTP"
+  port              = local.http_port
+  protocol          = "HTTP"
 
   default_action {
     type = "fixed-response"
@@ -123,7 +125,7 @@ resource "aws_lb_listener" "http" {
     fixed_response {
       content_type = "text/plain"
       message_body = "404: page not found"
-      status_code = 404
+      status_code  = 404
     }
   }
 }
@@ -131,17 +133,17 @@ resource "aws_lb_listener" "http" {
 resource "aws_lb_target_group" "asg" {
   name = "${var.cluster_name}-target-group"
 
-  port = var.server_port
+  port     = local.server_port
   protocol = "HTTP"
-  vpc_id = data.aws_vpc.default.id
+  vpc_id   = data.aws_vpc.default.id
 
   health_check {
-    path = "/"
-    protocol = "HTTP"
-    matcher = "200"
-    interval = 15
-    timeout = 3
-    healthy_threshold = 2
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
     unhealthy_threshold = 2
   }
 }
@@ -149,7 +151,7 @@ resource "aws_lb_target_group" "asg" {
 # config for listener rule for auto scale group
 resource "aws_lb_listener_rule" "asg" {
   listener_arn = aws_lb_listener.http.arn
-  priority = 1
+  priority     = 1
 
   condition {
     path_pattern {
@@ -158,13 +160,13 @@ resource "aws_lb_listener_rule" "asg" {
   }
 
   action {
-    type = "forward"
+    type             = "forward"
     target_group_arn = aws_lb_target_group.asg.arn
   }
 }
 
 
 output "alb_dns_name" {
-  value = aws_lb.ec2-grp.dns_name
+  value       = aws_lb.ec2-grp.dns_name
   description = "The domain name of the load balancer"
 }
