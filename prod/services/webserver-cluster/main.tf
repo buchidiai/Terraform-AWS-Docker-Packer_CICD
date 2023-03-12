4189b12380259d4187af8caee0ff62d9b0334dd3
buchidiai <31743169+buchidiai@users.noreply.github.com>
2023-03-11 18:12:15 -0600

    fix output bug & create prod env

---
+++ prod/services/webserver-cluster/main.tf
@@ -0,0 +1,34 @@
+#terraform settings
+terraform {
+  required_providers {
+
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+  required_version = "1.3.9" # or latest Terraform version
+}
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+terraform {
+  backend "s3" {
+    bucket = "masterclass-bucketo"
+    key    = "prod/services/webserver-cluster/terraform.tfstate"
+    region = "us-east-1"
+
+    dynamodb_table = "terraform-locks-table"
+    encrypt        = true
+  }
+}
+
+module "webserver_cluster" {
+  source = "../../../modules/services/webserver-cluster"
+
+  cluster_name  = "webserver-prod"
+  instance_type = "t2.micro"
+  min_size      = 2
+  max_size      = 10
+}
